9766b4a0bf62c5d54bc1b08c1566f57cf974e113
Margarita Kozlenko <33878459+margaretkk@users.noreply.github.com>
2021-12-09 00:39:01 +0300

    Add FileService

---
+++ FileService.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using System.Text;
+
+namespace WordsBreaker
+{
+    internal static class FileService
+    {
+        public static string[] ReadFile(string inputPath) => 
+            File.ReadAllLines(inputPath);
+
+        public static void OutputFile() =>
+            File.WriteAllLines("files/output.tsv", StringExtensions.WordsProcessing(), Encoding.UTF8);
+    }
+}
