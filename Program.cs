b2c356daa1411198f859468da3c420d54dbc6859
Margarita Kozlenko <33878459+margaretkk@users.noreply.github.com>
2021-12-10 02:35:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,15 +1,10 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-
 namespace WordsBreaker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            StringExtensions.WordsProcessing();
+            WordsBreaker.Instance.Run();
         }
     }
 }
